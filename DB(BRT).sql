f265c3bdb1c3931d8dffcfa80715e6078be46815
214cs02002408 <derrick.bansah@st.vvu.edu.gh>
2016-12-12 22:24:19 +0100

    Update DB(BRT).sql

--- DB(BRT).sql
+++ DB(BRT).sql
@@ -3,5 +3,6 @@ bu_id varchar(12) NOT NULL PRIMARY KEY,
 driver_id varchar(12) NOT NULL,
 cond_id varchar(12) NOT NULL,
 dest_from varchar(50) NOT NULL,
-dest_to varchar(50) NOT NULL
+dest_to varchar(50) NOT NULL,
+sales int(12) NOT NULL
 );
