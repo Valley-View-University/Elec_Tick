80eff3439f6ac1519e6560b9542bf659edaf7369
214cs02002408 <derrick.bansah@st.vvu.edu.gh>
2016-12-12 22:15:02 +0100

    new commit database

---
+++ DB(BRT).sql
@@ -0,0 +1,7 @@
+﻿create table Bus(
+bu_id varchar(12) NOT NULL PRIMARY KEY,
+driver_id varchar(12) NOT NULL,
+cond_id varchar(12) NOT NULL,
+dest_from varchar(50) NOT NULL,
+dest_to varchar(50) NOT NULL
+);
